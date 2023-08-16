821ea1b06861ec9bcea27ec32703b728b06480cb
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2023-08-16 19:28:32 +0100

    chore: TODOS defined

--- project/src/routes/assets.ts
+++ project/src/routes/assets.ts
@@ -2,8 +2,30 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ info: "App is running!" });
+router.get("", (req, res) => {
+  try {
+    //TODO: validate the id as valid uuid.
+    //TODO: get the asset by id.
+  } catch (error) {
+    console.error("Error encoding URL:", error);
+    return res
+      .status(500)
+      .json({ error: "An error occurred while storing asset." });
+  }
+  return;
+});
+
+router.post("", (req: Request, res: Response) => {
+  try {
+    //TODO: validate the body.
+    //TODO: save the asset.
+  } catch (error) {
+    console.error("Error encoding URL:", error);
+    return res
+      .status(500)
+      .json({ error: "An error occurred while storing asset." });
+  }
+  return;
 });
 
 export default router;
