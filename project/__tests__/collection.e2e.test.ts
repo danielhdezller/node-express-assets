b51e28e0bff68e89d4d82ff5e3c85db70b8c6124
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2023-08-18 17:14:07 +0100

    feat: e2e collections clean db after test

--- project/__tests__/collection.e2e.test.ts
+++ project/__tests__/collection.e2e.test.ts
@@ -21,10 +21,14 @@ describe("E2E Collection test.", () => {
   });
 
   it("Should get the list of collections.", async () => {
-    collection = collectionRepository.create({ name: "name" });
+    const name = "Pop";
+
+    collection = collectionRepository.create({ name });
     collection = await collectionRepository.save(collection);
     const res = await request(app).get("/collections");
     expect(res.status).toBe(200);
-    expect(res.body.length).toBeGreaterThanOrEqual(1);
+    expect(res.body.length).toBe(1);
+
+    await collectionRepository.delete({ name });
   });
 });
